6695447c7f7893a9d74bd098b13c907f88aa2a38
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-24 15:37:26 -0300

    refatorando codigo

--- inserindoDadosUsuario/booleanos/script.js
+++ inserindoDadosUsuario/booleanos/script.js
@@ -1,10 +1,9 @@
 let numtxt1 = document.querySelector('#num1txt')
 let numtxt2 = document.querySelector('#num2txt')
-function calcular(){
-    let num1 = numtxt1.value
-    let num2 = numtxt2.value
+function calcularMedia(){
+    let media = (numtxt1.value+numtxt2.value)/2
     let resExe1 = document.querySelector('#resExe01')
- if((num1+num2)/2>5){
+ if(media>5){
     resExe1.innerHTML = 'Aprovado'
  }else{
     resExe1.innerHTML = 'Reprovado'
