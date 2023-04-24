eb5ec3713b9cca2dbd8a050b2684d5a5944c9ba0
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-24 16:02:15 -0300

    variavel Media
    retirei a variável media da função, para ser usada em outra função no futuro

--- inserindoDadosUsuario/booleanos/script.js
+++ inserindoDadosUsuario/booleanos/script.js
@@ -1,8 +1,9 @@
 let numtxt1 = document.querySelector('#num1txt')
 let numtxt2 = document.querySelector('#num2txt')
+let resExe1 = document.querySelector('#resExe01')
+
 function calcularMedia(){
     let media = (numtxt1.value+numtxt2.value)/2
-    let resExe1 = document.querySelector('#resExe01')
  if(media>5){
     resExe1.innerHTML = 'Aprovado'
  }else{
