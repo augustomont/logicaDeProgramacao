35c505690ad3cf7d175d0e2f0c179ec8005a3010
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-02 14:07:22 -0300

    media prova

---
+++ inserindoDadosUsuario/mediaProva/script.js
@@ -0,0 +1,16 @@
+function avaliar(){
+    let p1 = document.querySelector('#p1txt')
+    let p2 = document.querySelector('#p2txt')
+    let media = (parseFloat(p1.value) + parseFloat(p2.value)) / 2
+    let res = document.querySelector('#resultado')
+
+    if(p1.value == 0 || p2.value == 0){
+        res.innerHTML = "REPROVADO!"
+    } else if(media<5){
+        res.innerHTML = `REPROVADO! <br>
+        Sua media é ${media}`
+    } else {
+        res.innerHTML = `APROVADO! <br>
+        Sua media é ${media}`
+    }
+}
